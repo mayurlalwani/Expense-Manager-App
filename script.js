d4f7a06e4b93524e7f3836e7cc77826d78f5f829
mayurlalwani <mayur_lalwani4@ymail.com>
2020-05-10 13:45:40 +0530

    Added Form Validation and split the code in different functions

--- script.js
+++ script.js
@@ -1,70 +1,63 @@
 let totalExpense=0;
 let totalIncome=0;
+
+const form = document.getElementById('form');
+const inputDate = document.querySelector('input[type="date"]');
+const inputDescription = document.querySelector('#description');
 const inputAmount = document.querySelector('#inputAmount');
-const form = document.getElementById("form_data");
 const expenseAmount = document.querySelector("#displayExpenseAmount");
 const incomeAmount = document.querySelector("#displayIncomeAmount");
 const list = document.getElementById('list');
-const expenseTable = document.querySelector("#expenseTable");
-const ul = document.querySelector('ul');
-const tableData = document.querySelector('tableData');
-const tableRow = document.querySelector('tableRow');
 const balance = document.querySelector("#balance");
-let bal = 0;
+let balanceTotal = 0;
 
 let allTransactions = [];
 const transaction = {};
 
-function addExpenseTotal(e){
-    
-    const inputDescription = document.querySelector('#description');
-    const description= inputDescription.value;
+function fetchValues(e){
+    e.preventDefault();
+    const description = inputDescription.value;
+    const date = (inputDate.value).toString();
+    const amount = parseInt(inputAmount.value,10);
+    if(date==="" || description==="" || amount===""){
+        alert("Enter Values");
+    }
+    addExpenseTotal(description, date, amount);
+}
 
-    const inputDateControl = document.querySelector('input[type="date"]');
-    const dateControl = inputDateControl.value;
-
-    const amount = inputAmount.value;
-
-    const amt = parseInt(amount,10);
-
-    transaction.date = dateControl;
+function addExpenseTotal(description, date, amount){    
+    transaction.date = date;
     transaction.desc = description;
-    transaction.amount = amt;
-
+    transaction.amount = amount;
     allTransactions.push(transaction);
 
     if(amount<0){
-        totalExpense+=amt;
+        totalExpense+=amount;
         expenseAmount.textContent = totalExpense;
     }
 
     else{
-        totalIncome+=amt;
+        totalIncome+=amount;
         incomeAmount.textContent = totalIncome;
-
     }
+
+    displayData();
+}
+
+function displayData(){
     const item = document.createElement('li');
-
-    bal = totalIncome + totalExpense;
-    balance.textContent = bal;
-
-    console.log(allTransactions);
-
+    balanceTotal = totalIncome + totalExpense;
+    balance.textContent = balanceTotal;
     const allExpenseHTML = allTransactions.map(exp => {
         return `<li> Date: ${exp.date} Transaction Type: ${exp.desc} Amount: ${exp.amount}</li>`
     });
-
-    // expenseTable.innerHTML = allExpenseHTML;
     item.innerHTML = allExpenseHTML;
     list.appendChild(item);
     inputAmount.value="";
     inputDescription.value="";
-    inputDateControl.value="";
+    inputDate.value="";
     allTransactions=[];
     return false
 }
 
-
-// const element = document.querySelector("#btnAddExpense");
-
-// element.addEventListener("click", addExpenseTotal, false);
+form.addEventListener("submit", fetchValues);
